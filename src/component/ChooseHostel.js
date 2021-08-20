c4e1410459d0663f45556ac2dcf1267205c98a0f
Rishabh gurjar <rishabhgurjar101@gmail.com>
2021-08-20 22:59:26 +0530

    first commit

--- src/component/ChooseHostel.js
+++ src/component/ChooseHostel.js
@@ -7,27 +7,27 @@ function ChooseHostel(){
         <div className="Container">
             <h1>Choose a Hostel</h1>
     <div className="gridHostel">
-        <Link to={"/floors/"+ value.name +1+"/"+value.username}> 
+        <Link to={"/floors/"+ value.name +"1/"+value.username}> 
               <button className="btnHostel">{value.name}1</button>
         </Link>
 
-        <Link to={"/floors/"+ value.name+1+"/"+value.username}> 
+        <Link to={"/floors/"+ value.name+"2/"+value.username}> 
         <button className="btnHostel">{value.name}2</button>
         </Link>
        
-        <Link to={"/floors/"+ value.name +1+"/"+value.username}> 
+        <Link to={"/floors/"+ value.name +"3/"+value.username}> 
            <button className="btnHostel">{value.name}3</button>
        </Link>
 
-        <Link to={"/floors/"+ "/"+value.username}> 
+        <Link to={"/floors/"+ value.name+"4/"+value.username}> 
         <button className="btnHostel">{value.name}4</button>
         </Link>
         
-        <Link to={"/floors/"+ value.name +1+"/"+value.username }> 
+        <Link to={"/floors/"+ value.name +"5/"+value.username }> 
         <button className="btnHostel">{value.name}5</button>
         </Link>
         
-        <Link to={"/floors/"+ value.name +1+"/"+value.username}> 
+        <Link to={"/floors/"+ value.name +"6/"+value.username}> 
         <button className="btnHostel">{value.name}6</button>
         </Link>
 
